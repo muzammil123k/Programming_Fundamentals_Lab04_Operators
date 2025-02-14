9301cd0a36e2949a402b83e7dd81606547f632d6
muzammil123k <114763950+muzammil123k@users.noreply.github.com>
2025-02-14 18:50:40 +0500

    Add files via upload

---
+++ i220970_Q2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"name:muzzamil ahmed \n rolno:i220970 \n ";
+const float pi=3.14;
+int rad;
+int hight;
+cout<<"plz enter raduis \n";
+cin>>rad;
+cout<<"plz enter height \n";
+cin>>hight;
+cout<<"\n perimeter:"<<2*pi*rad;
+cout<<"\n lateral surface area:"<<2*pi*rad*hight;
+cout<<"\n volume:"<<pi*rad*rad*hight;
+cout<<"\n";
+return 0;
+}
+
+
+
+
---
+++ i220970_Q4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"name:muzammil ahmed\nrollno:i220970";
+int miles=375;
+int gallons=15;
+cout<<"\nMPG="<<miles/gallons;
+cout<<"\n";
+return 0;
+}
